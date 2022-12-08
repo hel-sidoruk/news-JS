72e9a7f2bb42616602d754b0d9baadcc888e5523
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-12-08 08:13:57 +0300

    feat: add pagination logic

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -3,23 +3,42 @@ import Pagination from '../../utils/pagination';
 import './news.css';
 
 class News implements View<NewsItem> {
+  constructor(
+    private newsData: NewsItem[] = [],
+    private page: number = 1,
+    private limit = 10,
+    private newsContainer: HtmlEl = document.querySelector('.news')
+  ) {}
   draw(data: NewsItem[]): void {
-    const news: NewsItem[] = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+    this.newsData = data;
+    if (this.newsData.length) {
+      const pagination = new Pagination(this.newsData.length, this.limit);
+      const handleClick = (e: Event) => {
+        const element = e.target as HTMLElement;
+        if (!element.classList.contains('pagination__btn')) return;
+        const page = Number(element.textContent);
+        this.page = page;
+        element.parentElement?.replaceWith(pagination.pagesElement(page, handleClick));
+        this.render();
+      };
 
+      const paginationEl = pagination.pagesElement(1, handleClick);
+      this.render();
+      if (this.newsContainer?.querySelector('.pagination')) {
+        this.newsContainer?.querySelector('.pagination')?.remove();
+      }
+      this.newsContainer?.append(paginationEl);
+    }
+  }
+  render() {
+    console.log(this);
+    const coef = this.limit * (this.page - 1);
+    const news: NewsItem[] =
+      this.newsData.length >= this.limit
+        ? this.newsData.filter((_item, idx) => idx >= 0 + coef && idx < 10 + coef)
+        : this.newsData;
     const fragment: DocumentFragment = document.createDocumentFragment();
     const newsItemTemp: HTMLTemplateElement | null = document.querySelector('#newsItemTemp');
-    const newsElement: HtmlEl = document.querySelector('.news');
-
-    const pagination = new Pagination(100, 10);
-    const handleClick = (e: Event) => {
-      console.log('click');
-      const element = e.target as HTMLElement;
-      if (!element.classList.contains('pagination__btn')) return;
-      const page = Number(element.textContent);
-      newsElement?.replaceChildren(pagination.pagesElement(page, handleClick));
-    };
-    const paginationEl = pagination.pagesElement(1, handleClick);
-
     news.forEach((item: NewsItem, idx) => {
       const newsClone = newsItemTemp?.content.cloneNode(true) as HTMLElement;
 
@@ -33,11 +52,9 @@ class News implements View<NewsItem> {
       const newsMoreLink: HtmlEl = newsClone.querySelector('.news__read-more a');
 
       if (idx % 2 && newsItem) newsItem.classList.add('alt');
-
       if (newsPhoto) newsPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
       if (newsAuthor) newsAuthor.textContent = item.author || item.source.name;
       if (newsDate) newsDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
-
       if (newsTitle) newsTitle.textContent = item.title;
       if (newsSource) newsSource.textContent = item.source.name;
       if (newsContent) newsContent.textContent = item.description;
@@ -46,9 +63,10 @@ class News implements View<NewsItem> {
       fragment.append(newsClone);
     });
 
-    if (newsElement) newsElement.innerHTML = '';
-    newsElement?.append(fragment);
-    newsElement?.append(paginationEl);
+    const newsContent = this.newsContainer?.querySelector('.news__content') as HTMLElement;
+
+    newsContent.innerHTML = '';
+    newsContent.append(fragment);
   }
 }
 
